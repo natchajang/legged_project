b3b7b346ded17eb1fb1d32aac51605ec66ec5314
natchajang <natchajang22717@gmail.com>
2023-12-14 22:20:30 +0700

    add save log while training and post-train

--- legged_project/envs/anymal_c/overcome_box/anymal_c_box_config.py
+++ legged_project/envs/anymal_c/overcome_box/anymal_c_box_config.py
@@ -32,23 +32,23 @@ import math
 from legged_project.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobotCfgPPO
 
 class AnymalCBoxCfg( LeggedRobotCfg ):
-    class description():
-        name = 'Test_version'
     class env( LeggedRobotCfg.env ):
-        num_observations = 242 # default is 235 measure height = 187
+        num_observations = 52 # default is 235 measure height = 187 242
         num_envs = 4096       # number of environment default = 4096
         num_actions = 12      # number of action equal to Dof (control with actuator network)
         send_timeouts = True  # send time out information to the algorithm
         episode_length_s = 20 # episode length in seconds
-        env_spacing = 3.  # not used with heightfields/trimeshes 
+        env_spacing = 3.      # not used with heightfields/trimeshes
+        
+        num_steps_per_env = 24 # use for save log in each iteration (need to equal to nnum_steps_per_env in train config)
         
     class terrain( LeggedRobotCfg.terrain ):
         # measure terrain
-        measure_heights = True # get the measurement for being in obs
+        measure_heights = False # get the measurement for being in obs
         measured_points_x = [-0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8] # 1mx1.6m rectangle (without center line)
         measured_points_y = [-0.5, -0.4, -0.3, -0.2, -0.1, 0., 0.1, 0.2, 0.3, 0.4, 0.5]
         # terrain type
-        mesh_type = 'trimesh' # ['plane', 'box']
+        mesh_type = 'plane' # ['plane', 'box']
         terrain_kwargs = {'num_obs':{'max':30, 'step':50}, 'obs_height':{'max':0.25, 'step':0.05}, 
                           'obs_width':{'max':0.5, 'step':0.1}, 'obs_length':{'max':2, 'step':0.4}} 
                         # Dict of arguments for selected terrain
@@ -64,7 +64,7 @@ class AnymalCBoxCfg( LeggedRobotCfg ):
         restitution = 0.
         
         # for rough terrain except "plane" type
-        curriculum = True # for increase difficulty of obstcle (box size)
+        curriculum = False # for increase difficulty of obstcle (box size)
         selected = False # select a unique terrain type and pass all arguments
    
         max_init_terrain_level = 1 # starting curriculum state
@@ -137,7 +137,16 @@ class AnymalCBoxCfg( LeggedRobotCfg ):
         
     class commands( LeggedRobotCfg.commands ):
         curriculum = True # If true the tracking reward is above 80% of the maximum, increase the range of commands
-        max_curriculum = 2.
+        max_curriculum = 1.
+        
+        # limit update command range (use if curriculum is True)
+        step_vel = 0.1
+        step_height = 0.025
+        step_angle = 0.02*math.pi
+        max_vel = 1.3
+        min_height = 0.25
+        max_angle = 0.2*math.pi
+        
         num_commands = 6 # default: 1.lin_vel_x, 2.lin_vel_y,
                          #          3.base_height
                          #          4.base_roll 5.base_pitch 6.base_yaw
@@ -146,20 +155,32 @@ class AnymalCBoxCfg( LeggedRobotCfg ):
         heading_command = False # if true: compute ang vel command from heading error (not use in our task)
         
         class ranges:           # range of command
-            lin_vel_x = [0.0, 0.7] # min max [m/s]
-            lin_vel_y = [-0.1, 0.1]   # min max [m/s]
-            base_height = [0.25, 0.50] # min max [m]
-            base_roll = [-0.1*math.pi, 0.1*math.pi]   # min max [rad]
-            base_pitch = [-0.05*math.pi, 0.05*math.pi]  # min max [rad]
-            base_yaw = [-0.1*math.pi, 0.1*math.pi]    # min max [rad]
+            # lin_vel_x = [-1.3, 1.3] # min max [m/s]
+            # lin_vel_y = [-1.3, 1.3]   # min max [m/s]
+            # base_height = [0.25, 0.50] # min max [m]
+            # base_roll = [-0.2*math.pi, 0.2*math.pi]   # min max [rad]
+            # base_pitch = [-0.2*math.pi, 0.2*math.pi]  # min max [rad]
+            # base_yaw = [-0.2*math.pi, 0.2*math.pi]    # min max [rad]
+            
+            start_vel = 0.5
+            start_height = 0.4
+            start_angle = 0.06
+            lin_vel_x = [-start_vel, start_vel] # min max [m/s]
+            lin_vel_y = [-start_vel, start_vel]   # min max [m/s]
+            base_height = [start_height, 0.50] # min max [m]
+            base_roll = [-start_angle*math.pi, start_angle*math.pi]   # min max [rad]
+            base_pitch = [-start_angle*math.pi, start_angle*math.pi]  # min max [rad]
+            base_yaw = [-start_angle*math.pi, start_angle*math.pi]    # min max [rad]
         
     class rewards( LeggedRobotCfg.rewards ):
-        only_positive_rewards = True # if true negative total rewards are clipped at zero (avoids early termination problems)
-        
+        only_positive_rewards = False # if true negative total rewards are clipped at zero (avoids early termination problems)
+        # Set reward tracking function types
+        reward_tracking = 'gaussian' # str ['binary', 'progress_estimator', 'gaussian']
+        reward_tracking_accept = {'velocity': 0.3, 'height':0.02, 'orientation':0.03*math.pi}
         # Need to tune
         tracking_sigma = 0.1 # tracking reward = exp(-error^2/sigma) for linear velocity
-        tracking_height = 0.025 # tracking reward = exp(-error^2/sigma) for height
-        tracking_orientation = 0.025 # tracking reward = exp(-error^2/sigma) for base orientation
+        tracking_height = 0.01 # tracking reward = exp(-error^2/sigma) for height
+        tracking_orientation = 0.01 # tracking reward = exp(-error^2/sigma) for base orientation
         
         soft_dof_pos_limit = 1. # percentage of urdf limits, values above this limit are penalized
         soft_dof_vel_limit = 1.
@@ -169,20 +190,21 @@ class AnymalCBoxCfg( LeggedRobotCfg ):
         
         class scales( LeggedRobotCfg.rewards.scales ):
             # pre defined reward function
-            tracking_lin_vel = 1.0
             lin_vel_z = -2.0
             ang_vel_xy = -0.05
             torques = -0.00001
             dof_acc = -2.5e-7
+            action_rate = -0.01
             feet_air_time = 1.0
             collision = -1.
-            action_rate = -0.01
             
             # add my own reward functions
+            tracking_lin_vel = 1.0
             tracking_height = 1.0
-            tracking_orientation = -1.0
+            tracking_orientation = 1.0
             
             # unenble some reward functions
+            action_rate = 0
             termination = -0.0
             tracking_ang_vel = 0.
             base_height = 0. # unused fix base height reward
@@ -192,9 +214,42 @@ class AnymalCBoxCfg( LeggedRobotCfg ):
             orientation = -0.
             
 class AnymalCBoxCfgPPO( LeggedRobotCfgPPO ):
-    class runner( LeggedRobotCfgPPO.runner ):
-        run_name = ''
-        experiment_name = 'box_anymal_c'
-        load_run = -1
+    seed = 1
+    runner_class_name = 'OnPolicyRunner'
+    class policy( LeggedRobotCfgPPO.policy ):
+        init_noise_std = 1.0
+        actor_hidden_dims = [512, 256, 128]
+        critic_hidden_dims = [512, 256, 128]
+        activation = 'elu' # can be elu, relu, selu, crelu, lrelu, tanh, sigmoid
+    
+    class algorithm ( LeggedRobotCfgPPO.algorithm ):
+        # training params
+        value_loss_coef = 1.0
+        use_clipped_value_loss = True
+        clip_param = 0.2
+        entropy_coef = 0.01
+        num_learning_epochs = 5
+        num_mini_batches = 4 # mini batch size = num_envs*nsteps / nminibatches
+        learning_rate = 1.e-3 #5.e-4
+        schedule = 'adaptive' # could be adaptive, fixed
+        gamma = 0.99
+        lam = 0.95
+        desired_kl = 0.01
+        max_grad_norm = 1.
         
-        max_iterations = 1500 # number of policy updates
+    class runner( LeggedRobotCfgPPO.runner ):
+        policy_class_name = 'ActorCritic'
+        algorithm_class_name = 'PPO'
+        num_steps_per_env = 24 # per iteration
+        max_iterations = 1000 # number of policy updates
+        
+        # logging
+        save_interval = 50 # check for potential saves every this many iterations
+        run_name = 'ex2_with_curriculum'               # sub experiment of each domain => save as name of folder
+        experiment_name = 'anymal_c_box' # domain of experiment
+        
+        # load and resume
+        resume = False
+        load_run = -1 # -1 = last run
+        checkpoint = -1 # -1 = last saved model
+        resume_path = None # updated from load_run and chkpt
--- legged_project/scripts/play_loadobj.py
+++ legged_project/scripts/play_loadobj.py
@@ -34,16 +34,37 @@ sys.path.append("/home/natcha/github/legged_project")
 
 from legged_project import LEGGED_GYM_ROOT_DIR
 import os
-
+import math
+import importlib
+import inspect
+import time
 import isaacgym
-from legged_project.envs import *
-from legged_project.utils import  get_args, export_policy_as_jit, task_registry, Logger
+from isaacgym import gymutil
+import legged_project
+from legged_project import envs
+from legged_project import utils
+from legged_project.envs import * 
+from legged_project.utils import  get_args, export_policy_as_jit, Logger, helpers, TaskRegistry
 
+import pickle
 import numpy as np
 import torch
 
 def play(args):
+    load_dir = os.path.join(LEGGED_GYM_ROOT_DIR, 'logs', args.task)
+    load_folder = [f for f in os.listdir(load_dir) if args.run_name in f]
+    if load_folder == []:
+        print("Error; not found that run_name in {} task".format(args.task))
+        return
+    elif len(load_folder) > 1:
+        print("Warning; found that run_name in {} task more than 1 !!!".format(args.task))
+    args.load_run = load_folder[0]
+    load_path = os.path.join(load_dir, args.load_run)
+    
     env_cfg, train_cfg = task_registry.get_cfgs(name=args.task)
+    #write over config from saved file
+    helpers.rideover_cfg(load_path, env_cfg, train_cfg)
+
     # override some parameters for testing
     env_cfg.env.num_envs = min(env_cfg.env.num_envs, 50) # limit number of visualization environments
     env_cfg.terrain.num_rows = 1
@@ -53,9 +74,47 @@ def play(args):
     env_cfg.domain_rand.randomize_friction = False
     env_cfg.domain_rand.push_robots = False
     
+    env_cfg.env.episode_length_s = 20
+    
     # prepare environment
     env, _ = task_registry.make_env(name=args.task, args=args, env_cfg=env_cfg)
     obs = env.get_observations()
+    
+    # set needed variable
+    logger = Logger(env.dt)
+    number_iter = 1
+    episode_length = 1000
+    robot_index = 0 # which robot is used for logging
+    joint_index = 1 # which joint is used for logging
+    stop_state_log = 100 # number of steps before plotting states
+    stop_rew_log = env.max_episode_length + 1 # number of steps before print average episode rewards
+    
+    #Add logger for save and analysis
+    logger_save = Logger(env.dt)
+    logger_save.log_state('dt', env.dt)
+    logger_save.log_state('num_samples', number_iter*episode_length)
+    
+    # change command if set desired command
+    if args.command_set != None:
+        env.command_set_flag =True
+        command_set = helpers.comand_set_parse(args.command_set)
+        list_timestamp = []
+        list_commands = []
+        command_idx = 0
+        episode_length = 0
+        for c in command_set:
+            # add time stamp to switch commands
+            episode_length += int(c[-1]/env.dt)
+            list_timestamp.append(episode_length)
+            c = [e*math.pi/180 if i>2 and i<6 else e for i,e in enumerate(c)][:-1]
+            c[0] = c[0]/100
+            c[1] = c[1]/100
+            c[2] = c[2]/100
+            command = torch.tensor(c, device=env.device, requires_grad=False)
+            command = command.repeat(env_cfg.env.num_envs, 1)
+            list_commands.append(command)
+        env.cfg.commands.resampling_time = 2*int(episode_length/env.dt)
+        env.command_own = list_commands[command_idx]
     
     # Visulization setting
     # add open debug_viz
@@ -66,27 +125,39 @@ def play(args):
         env.commands_viz = True
         
     # load policy
-    train_cfg.runner.resume = True
+    train_cfg.runner.resume = True # use for load previous model for continue training or load model for playing by specifi checkpoint and load_run
     ppo_runner, train_cfg = task_registry.make_alg_runner(env=env, name=args.task, args=args, train_cfg=train_cfg)
     policy = ppo_runner.get_inference_policy(device=env.device)
+    
+    # print env and policy info form configuration
+    print("replay => run name: {}------------------------".format(train_cfg.runner.run_name))
+    print("State space\n num_observations: {}\n measure height: {}\n".format(env_cfg.env.num_observations, env_cfg.terrain.measure_heights))
+    # print("Reward\n function type:{}".format(env_cfg.terrain.mesh_type))
+    print("Terrain\n mesh type: {}\n ".format(env_cfg.terrain.mesh_type))
+    print("Techniuqe\n terrain cur: {}\n command cur: {}\n positive rew: {}".format(env_cfg.terrain.curriculum,
+                                                                                    env_cfg.commands.curriculum,
+                                                                                    env_cfg.rewards.only_positive_rewards))
+    print("Load run\n run: {}\n model: {}".format(train_cfg.runner.load_run, train_cfg.runner.checkpoint))
+    print("----------------------------------------------")
     
     # export policy as a jit module (used to run it from C++)
     if EXPORT_POLICY:
         path = os.path.join(LEGGED_GYM_ROOT_DIR, 'logs', train_cfg.runner.experiment_name, 'exported', 'policies')
         export_policy_as_jit(ppo_runner.alg.actor_critic, path)
         print('Exported policy as jit script to: ', path)
-
-    logger = Logger(env.dt)
-    robot_index = 0 # which robot is used for logging
-    joint_index = 1 # which joint is used for logging
-    stop_state_log = 100 # number of steps before plotting states
-    stop_rew_log = env.max_episode_length + 1 # number of steps before print average episode rewards
+    
     camera_position = np.array(env_cfg.viewer.pos, dtype=np.float64)
     camera_vel = np.array([1., 1., 0.])
     camera_direction = np.array(env_cfg.viewer.lookat) - np.array(env_cfg.viewer.pos)
     img_idx = 0
 
-    for i in range(10*int(env.max_episode_length)):
+    for i in range(number_iter*int(episode_length)):
+
+        if args.command_set != None:
+            if i==list_timestamp[command_idx]:
+                command_idx+=1
+                env.command_own = list_commands[command_idx]
+                
         actions = policy(obs.detach())
         obs, _, rews, dones, infos = env.step(actions.detach())
         
@@ -98,37 +169,39 @@ def play(args):
         if MOVE_CAMERA:
             camera_position += camera_vel * env.dt
             env.set_camera(camera_position, camera_position + camera_direction)
-
-        if i < stop_state_log:
-            logger.log_states(
+        
+        logger_save.log_states(
                 {
                     'dof_pos_target': actions[robot_index, joint_index].item() * env.cfg.control.action_scale,
                     'dof_pos': env.dof_pos[robot_index, joint_index].item(),
                     'dof_vel': env.dof_vel[robot_index, joint_index].item(),
                     'dof_torque': env.torques[robot_index, joint_index].item(),
-                    'command_x': env.commands[robot_index, 0].item(),
-                    'command_y': env.commands[robot_index, 1].item(),
-                    'command_yaw': env.commands[robot_index, 2].item(),
+                    'command_lin_x': env.commands[robot_index, 0].item(),
+                    'command_lin_y': env.commands[robot_index, 1].item(),
+                    'command_height': env.commands[robot_index, 2].item(),
+                    'command_roll': env.commands[robot_index, 3].item(),
+                    'command_pitch': env.commands[robot_index, 4].item(),
+                    'command_yaw': env.commands[robot_index, 5].item(),
+                    'base_roll' : env.base_euler[robot_index, 0].item(),
+                    'base_pitch' : env.base_euler[robot_index, 1].item(),
+                    'base_yaw' : env.base_euler[robot_index, 2].item(),
+                    'base_pos_x' : env.base_mean_height[robot_index].item(),    
                     'base_vel_x': env.base_lin_vel[robot_index, 0].item(),
                     'base_vel_y': env.base_lin_vel[robot_index, 1].item(),
                     'base_vel_z': env.base_lin_vel[robot_index, 2].item(),
                     'base_vel_yaw': env.base_ang_vel[robot_index, 2].item(),
-                    'contact_forces_z': env.contact_forces[robot_index, env.feet_indices, 2].cpu().numpy()
+                    'contact_forces_z': env.contact_forces[robot_index, env.feet_indices, 2].cpu().numpy(),
+                    'distance_x' : env.root_states[robot_index, 0].item(),
+                    'distance_y' : env.root_states[robot_index, 1].item()
                 }
             )
-        elif i==stop_state_log:
-            logger.plot_states()
-        if  0 < i < stop_rew_log:
-            if infos["episode"]:
-                num_episodes = torch.sum(env.reset_buf).item()
-                if num_episodes>0:
-                    logger.log_rewards(infos["episode"], num_episodes)
-        elif i==stop_rew_log:
-            logger.print_rewards()
+    
+    helpers.save_log(logger_save.state_log, load_path, 'state_log.pkl')
 
 if __name__ == '__main__':
-    EXPORT_POLICY = True
+    EXPORT_POLICY = False
     RECORD_FRAMES = False
     MOVE_CAMERA = False
     args = get_args()
     play(args)
+    
